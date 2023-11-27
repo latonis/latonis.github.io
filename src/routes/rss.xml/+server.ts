ebd55958f9dc1abefd059a81aa821772c7671d9d
Jacob Latonis <jlatonis@me.com>
2023-11-27 12:21:52 -0600

    fix rss

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -3,12 +3,12 @@ import type { Post } from '$lib/types'
 export const prerender = true
 
 export async function GET({ fetch }) {
-	const response = await fetch('api/posts')
-	const posts: Post[] = await response.json()
+    const response = await fetch('api/posts')
+    const posts: Post[] = await response.json()
 
-	const headers = { 'Content-Type': 'application/xml' }
+    const headers = { 'Content-Type': 'application/xml' }
 
-	const xml = `
+    const xml = `
 		<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
 			<channel>
 				<title>${config.title}</title>
@@ -16,21 +16,21 @@ export async function GET({ fetch }) {
 				<link>${config.url}</link>
 				<atom:link href="${config.url}rss.xml" rel="self" type="application/rss+xml"/>
 				${posts
-					.map(
-						(post) => `
+            .map(
+                (post) => `
 						<item>
 							<title>${post.title}</title>
 							<description>${post.description}</description>
-							<link>${config.url}/${post.slug}</link>
-							<guid isPermaLink="true">${config.url}/${post.slug}</guid>
+							<link>${config.url}${post.slug}</link>
+							<guid isPermaLink="true">${config.url}${post.slug}</guid>
 							<pubDate>${new Date(post.date).toUTCString()}</pubDate>
 						</item>
 					`
-					)
-					.join('')}
+            )
+            .join('')}
 			</channel>
 		</rss>
 	`.trim()
 
-	return new Response(xml, { headers })
+    return new Response(xml, { headers })
 }
