da360e1f721fcfeb7a1497650243337b28fd39fa
Jacob Latonis <jlatonis@me.com>
2023-11-30 18:06:46 -0600

    Updates

---
+++ _app/immutable/chunks/paths.1708ebaf.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_17cob2z)==null?void 0:s.base)??"/latonis.github.io";var t;const e=((t=globalThis.__sveltekit_17cob2z)==null?void 0:t.assets)??a;export{e as a,a as b};
--- _app/immutable/chunks/paths.7217e415.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1z0y4xq)==null?void 0:s.base)??"/latonis.github.io";var t;const e=((t=globalThis.__sveltekit_1z0y4xq)==null?void 0:t.assets)??a;export{e as a,a as b};
